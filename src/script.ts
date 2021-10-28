e50063ab2f7a361f50aaa11ccf12aec786420567
nir <nira@moveo.co.il>
2021-10-28 14:33:14 +0300

    adding documentation and create new interface

--- src/script.ts
+++ src/script.ts
@@ -3,52 +3,83 @@ import NewsAPI from 'ts-newsapi';
 import { SourcesHandler } from './api/v1/sources/sources.handler';
 import { TopHeadlinesHandler } from './api/v1/top-headlines/top-headlines.handler';
 import * as _ from 'lodash';
-import { ApiNewsLanguage, INewsApiArticle, INewsApiEverythingParams, INewsApiSourceItem } from 'ts-newsapi/lib/types';
+import { ApiNewsCategory, ApiNewsCountry, ApiNewsLanguage, INewsApiArticle, INewsApiEverythingParams, INewsApiSource, INewsApiSourceItem } from 'ts-newsapi/lib/types';
+
+
+// new interface to add varubals to article schema
+interface newArticleInterface extends INewsApiSource{
+
+  language?:ApiNewsLanguage;
+
+  country?:ApiNewsCountry;
+
+  category?:ApiNewsCategory;
+}
 
 const newsapi= new NewsAPI(process.env.NEWS_API_KEY);
+
 class Api {
-  compare(from:INewsApiSourceItem,to:INewsApiArticle){
-    to.source.language = from.language;
-    to.source.category = from.category;
-    to.source.country = from.country;
+  /**
+   * the function add data from original source item to arctical new item
+   * @param from - original source
+   * @param to - new interfacr of source (include: language, category and country)
+   */
+  compare(from:INewsApiSourceItem,to:newArticleInterface){
+    to.language = from.language;
+    to.category = from.category;
+    to.country = from.country;
   }
 
   async getData(){
+    //get all sources from news Api
     const ApiSources= await newsapi.getSources();
+
     const sourcesHandler = new SourcesHandler;
     const everyThingHandler = new EverythingHandler;
     const topHeadlinesHandler= new TopHeadlinesHandler;
+
+    //add sourced form Api to DB
     sourcesHandler.upsertMany("id",ApiSources.sources);
-    let sourcesArr:string[] = [];
-    let completeEverything:INewsApiArticle[]=[];
-    let completeTopHedline:INewsApiArticle[]=[];
-    ApiSources.sources.forEach((source)=>{
-      sourcesArr.push(source.id)
-    });
+
+    //create sources id array
     const sourceDic= _.mapKeys(ApiSources.sources,"id");
+    const surcesID= Object.keys(sourceDic);
+
+    //create filters params for Api request
     const params :INewsApiEverythingParams ={
-      sources:sourcesArr,
+      sources:surcesID,
       pageSize:100
     };
     
+
+    // get all arcticals of everything and top-headlines 
     await Promise.all([   
+
+      // get all everything arcticles from Api
       newsapi.getEverything(params).then((res)=>{
-          completeEverything= res.articles.map((everything)=>{
-            this.compare(sourceDic[`${everything.source.id}`],everything)
-         return everything;
+          return res.articles.map((res)=>{
+            const everyting :newArticleInterface= res.source
+            this.compare(sourceDic[`${res.source.id}`],everyting)
+         return res;
        })  
-     }).then(()=>{
-      everyThingHandler.upsertMany("url",completeEverything);
+     }).then((result)=>{
+
+     // add all everything arcticles from Api to DB
+      everyThingHandler.upsertMany("url",result);
     }).catch((err)=>{
        console.log(err);     
      }),
+
+      // get all top-headline arcticles from Api
      newsapi.getTopHeadlines(params).then((res)=>{
-      completeTopHedline= res.articles.map((top)=>{
-        this.compare(sourceDic[`${top.source.id}`],top)
-        return top;
+      return res.articles.map((res)=>{
+        const top:newArticleInterface= res.source
+        this.compare(sourceDic[`${res.source.id}`],top)
+        return res;
      })
-   }).then(()=>{
-       topHeadlinesHandler.upsertMany("url",completeTopHedline);
+   }).then((result)=>{
+     // add all top-headline arcticles from Api to DB
+     topHeadlinesHandler.upsertMany("url",result);
      }).catch((err)=>{
      console.log(err);     
    })
