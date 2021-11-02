d1e18b14cc21e3017cd55d4b82dd57ef5cd7d010
nir <nira@moveo.co.il>
2021-11-02 11:46:59 +0200

    add function to route

--- src/api/v1/top-headlines/top-headlines.route.ts
+++ src/api/v1/top-headlines/top-headlines.route.ts
@@ -5,7 +5,13 @@ import { Request, Response, NextFunction, Router } from 'express';
 export class topHeadlinesRouter extends BaseRoute<topHeadlinesController> {
 
     protected initializeRoutes() {
-
+        this.router.post('/getAll', (request: Request, response: Response, next: NextFunction) => this.controller.search(request,response,next));
+        this.router.post('/getCategories',(request: Request, response: Response, next: NextFunction) => this.controller.getDropdown(request,response,next,"category"));
+        this.router.post('/getCountries',(request: Request, response: Response, next: NextFunction) => this.controller.getDropdown(request,response,next,"country"));
+        this.router.post('/getLanguages',(request: Request, response: Response, next: NextFunction) => this.controller.getDropdown(request,response,next,"language"));
+        this.router.post('/getSources',(request: Request, response: Response, next: NextFunction) => this.controller.getDropdown(request,response,next,"name"));
+        this.router.post('/getSourcesStatistics',(request: Request, response: Response, next: NextFunction) => this.controller.getSourceStatistics(request,response,next));
+        this.router.post('/getDatesStatistics',(request: Request, response: Response, next: NextFunction) => this.controller.getDatesStatistics(request,response,next));
     }
 
     protected getController(): topHeadlinesController {
